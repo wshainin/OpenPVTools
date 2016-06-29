f349b670e9880e9a8f198f2f4edceb6bfaa0cf16
wshainin <wshainin@gmail.com>
2016-06-29 10:45:16 -0600

    Update plotting function

--- octave/plotRecon.m
+++ octave/plotRecon.m
@@ -4,8 +4,7 @@ function plotRecon(imagePVP, reconPVP)
    image = readpvpfile(imagePVP);
    recon = readpvpfile(reconPVP);
    frames = min(numel(recon), numel(image));
-   %%for i = 1:frames
-   for i = 800*8+399:800*8+400
+   for i = 1:frames
       img = (permute(imadjust(image{i}.values, [min(image{i}.values(:)), max(image{i}.values(:))]), [2,1,3]));
       rec = (permute(imadjust(recon{i}.values, [min(recon{i}.values(:)), max(recon{i}.values(:))]), [2,1,3]));
       comb = imresize(horzcat(img, rec), 2);
